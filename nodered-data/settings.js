d269db0d901f3bcdd50c6dc236e431185affabee
Stefano Del Gobbo Acciarretti <stefano.delgobbo@gmail.com>
2025-12-23 12:44:27 +0100

    Create settings.js

---
+++ nodered-data/settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  adminAuth: {
+    type: "credentials",
+    users: [{
+      username: "admin",
+      password: "$2b$08$INCOLLA_HASH_GENERATO",
+      permissions: "*"
+    }]
+  }
+
+}
